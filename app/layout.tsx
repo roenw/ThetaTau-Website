4632343aaca474a906e2f7b38065a2f6d137f85b
Roen Wainscoat <13713430+roenw@users.noreply.github.com>
2024-02-25 20:04:45 -0700

    fix problem with vercel not loading navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
-import Navbar from "../components/navbar";
+import Navbar from "../components/Navbar.js";
 
 const inter = Inter({ subsets: ["latin"] });
 
