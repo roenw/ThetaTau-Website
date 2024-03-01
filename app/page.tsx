6bfd32ddada45977a83af8237faf875630d81c2a
Garrett Gibson <151679214+GarrettSG@users.noreply.github.com>
2024-02-29 22:42:12 -0700

    Homepage Image. Broken

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,15 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
+      <div style={{width: '100%', height: '100%', position: 'relative'}}>
+        <Image
+          // className="object-fill"
+          src="/TempeSkyline.jpg"
+          layout='fill'
+          objectFit='contain'
+          alt="Tempe Picture"
+        />
+      </div>
     </main>
   );
 }
