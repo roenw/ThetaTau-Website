6e09e6a76d97f4cfdec3f9ea988eb1ec6c90cbce
Roen Wainscoat <13713430+roenw@users.noreply.github.com>
2024-03-25 23:18:43 -0700

    center align good or bad?

--- app/page.tsx
+++ app/page.tsx
@@ -16,37 +16,36 @@ export default function Home() {
           layout="fill" // Fill the container
           objectFit="cover" // Cover the container while maintaining aspect ratio
           alt="Tempe Picture" // Alt text for accessibility
-          style={{ filter: 'brightness(50%)' }} // Lower opacity to 70%
+          style={{ filter: 'brightness(38%)' }} // Lower opacity to 70%
         />
-        {/* Text overlay */}
-        <div
-          style={{
-            position: 'absolute',
-            top: '200px', // Adjust as needed
-            left: '8vw', // Adjust as needed
-            color: 'white',
-            fontSize: '75px', // Relative to viewport width (adjust as needed)
-            fontFamily: 'Garamond', // Professional font family
-            fontWeight: 'bold',
-            backgroundColor: 'rgba(0, 0, 0, 0)',
-          }}
-        >
-          Theta Tau
-        </div>
+          {/* Text overlay */}
+          <div
+            className="text-center text-[75px] lg:text-[90px] mx-15"
+            style={{
+              position: 'relative',
+              top: '200px', // Adjust as needed
+              color: 'white',
+              fontFamily: 'Cambria', // Professional font family
+              fontWeight: 'bold',
+              backgroundColor: 'rgba(0, 0, 0, 0)',
+            }}
+          >
+            Theta Tau
+          </div>
 
-        {/* Tagline */}
-        <div
-          style={{
-            position: 'absolute',
-            top: '300px', // Relative to viewport height (adjust as needed)
-            left: '8vw', // Relative to viewport width (adjust as needed)
-            color: 'white',
-            fontFamily: 'Garamond', // Professional font family
-            fontSize: '30px', // Relative to viewport width (adjust as needed)
-          }}
-        >
-          Professional Co-ed Engineering Fraternity
-        </div>
+          {/* Tagline */}
+          <div
+            className="text-center mx-15"
+            style={{
+              position: 'relative',
+              top: '185px', // Relative to viewport height (adjust as needed)
+              color: 'white',
+              fontFamily: 'Cambria', // Professional font family
+              fontSize: '32px', // Relative to viewport width (adjust as needed)
+            }}
+          >
+            Professional Co-ed Engineering Fraternity at Arizona State University
+          </div>
       </div>
     </main>
   );
